0912ea962af5be3a39c75f0a209fc94ae3076c89
Nazar Vinnichuk <nazar@vinnich.uk>
2025-02-16 15:34:59 +0200

    Add data preparation code.

---
+++ dataset.py
@@ -0,0 +1,43 @@
+from sentencepiece import SentencePieceProcessor
+import torch
+
+
+class TranslationDataset(torch.utils.data.Dataset):
+    def __init__(self, in_lang, out_lang, sequence_length):
+        super().__init__()
+        self.in_processor = SentencePieceProcessor()
+        self.out_processor = SentencePieceProcessor()
+        self.in_processor.Load(f"{in_lang}.model")
+        self.out_processor.Load(f"{out_lang}.model")
+        self.in_data = read_lines(in_lang)
+        self.out_data = read_lines(out_lang)
+        self.sequence_length = sequence_length
+
+    def __len__(self):
+        return len(self.in_data)
+
+    def __getitem__(self, index):
+        in_sentence = self.fix_length(
+            self.in_processor.EncodeAsIds(self.in_data[index].strip())
+        )
+        out_sentence_bos = self.fix_length(
+            [1] + self.out_processor.EncodeAsIds(self.out_data[index].strip())
+        )
+        out_sentence_eos = self.fix_length(
+            self.out_processor.EncodeAsIds(self.out_data[index].strip()) + [2]
+        )
+        return in_sentence, out_sentence_bos, out_sentence_eos
+
+    def fix_length(self, tokens):
+        if len(tokens) < self.sequence_length:
+            return tokens + [0] * (self.sequence_length - len(tokens))
+        return tokens[: self.sequence_length]
+
+
+def read_lines(path):
+    with open(path) as file:
+        return file.readlines()
+
+
+def get_words(path):
+    return [line.strip().split("\t")[0] for line in read_lines(path)]
---
+++ prepare_data.py
@@ -0,0 +1,7 @@
+from sentencepiece import SentencePieceTrainer
+
+for lang in ["de", "en"]:
+    SentencePieceTrainer.Train(
+        f"--input={lang} --model_prefix={lang} --vocab_size=1024 --model_type=unigram "
+        "--pad_id=0 --bos_id=1 --eos_id=2 --unk_id=3 --character_coverage=1.0"
+    )
