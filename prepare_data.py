e018190b4ea60f8a07f953f747f6cf3cbfde359a
Nazar Vinnichuk <nazar@vinnich.uk>
2025-02-16 20:35:29 +0200

    Increase the size of the vocabulary.

--- prepare_data.py
+++ prepare_data.py
@@ -2,6 +2,6 @@ from sentencepiece import SentencePieceTrainer
 
 for lang in ["de", "en"]:
     SentencePieceTrainer.Train(
-        f"--input={lang} --model_prefix={lang} --vocab_size=4096 --model_type=unigram "
+        f"--input={lang} --model_prefix={lang} --vocab_size=10000 --model_type=unigram "
         "--pad_id=0 --bos_id=1 --eos_id=2 --unk_id=3 --character_coverage=1.0"
     )
